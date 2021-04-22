c142ab9dbd9a8f89ae5389d1aab7cdd7d64a8672
Jeffrey Zheng <jeffrey.zheng@sfasu.edu>
2021-04-22 09:20:48 -0500

    Add project files.

---
+++ TestApp/Customers.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Customers.aspx.cs" Inherits="TestApp.Customers" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h3>Customers</h3>
+    <asp:Label ID="lblCustomerCount" runat="server" ></asp:Label>
+    
+    <br />
+    <asp:Table ID="tblCustomers" runat="server" CssClass="table table-stripe">
+        <asp:TableHeaderRow>
+            <asp:TableHeaderCell>CustomerID</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Customer Name</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Contact Name</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Address</asp:TableHeaderCell>
+            <asp:TableHeaderCell>City</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Postal Code</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Country</asp:TableHeaderCell>
+        </asp:TableHeaderRow>
+
+    </asp:Table>
+    
+</asp:Content>
---
+++ TestApp/Customers.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+namespace TestApp
+{
+    public partial class Customers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                // 1. Create a database connection object
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = WebConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+
+                // you can also hard-code your connection string like the following:
+                // conn.ConnectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename =|DataDirectory|\Northwind.mdf; Integrated Security = True; Connect Timeout = 30";
+                // 2. Create a database command object 
+                SqlCommand cmd = new SqlCommand();
+
+                // 3. Assign a SQL query to the command object
+                cmd.CommandText = "SELECT * FROM Customers";
+                cmd.Connection = conn;
+                // 4. Connect to the database
+                conn.Open();
+
+                // 5. Execute the query using the command object
+                // 6. The command object will return a dataset that contains your data
+                SqlDataReader reader = cmd.ExecuteReader();
+                // 7. Finally, we need to discconect from the database
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        TableRow tr = new TableRow();
+                        TableCell tc = new TableCell();
+                        tc.Text = reader["CustomerID"].ToString();
+                        tr.Cells.Add(tc);
+
+                        tc = new TableCell();
+                        tc.Text = reader["CustomerName"].ToString();
+                        tr.Cells.Add(tc);
+
+                        tc = new TableCell();
+                        tc.Text = reader["ContactName"].ToString();
+                        tr.Cells.Add(tc);
+
+                        tc = new TableCell();
+                        tc.Text = reader["Address"].ToString();
+                        tr.Cells.Add(tc);
+
+                        tc = new TableCell();
+                        tc.Text = reader["City"].ToString();
+                        tr.Cells.Add(tc);
+
+                        tc = new TableCell();
+                        tc.Text = reader["PostalCode"].ToString();
+                        tr.Cells.Add(tc);
+
+                        tc = new TableCell();
+                        tc.Text = reader["Country"].ToString();
+                        tr.Cells.Add(tc);
+
+                        tblCustomers.Rows.Add(tr);
+                    }
+                }
+            }
+        }
+    }
+}
